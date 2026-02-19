20eb492a0a2b19e0c0e6e4795bff42b350665a45
Hister <jistahj@gmail.com>
2026-02-18 23:24:41 -0300

    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,20 @@ import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-    image: {
+  image: {
     service: { entrypoint: 'astro/assets/services/noop' }
   },
   integrations: [tailwind()],
   output: 'static',
-  adapter: vercel(),
+  adapter: vercel({
+    imageService: false  // ✅ DESACTIVA optimización de imágenes
+  }),
   vite: {
     build: {
       minify: 'terser',
       terserOptions: {
         compress: {
-          drop_console: true, // ✅ Elimina console.log en producción
+          drop_console: true,
         },
       },
     },
